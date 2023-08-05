64f4c63f53e101d3c740b72ef9be89c063d10768
akshaychavan010101 <akshaychavan010101@gmail.com>
2023-08-05 11:02:20 +0530

    ready to deploy

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const connection = require("./db");
+const connection = require("./config/db");
 require("dotenv").config();
 import UserRouter from "./routes/users.routes";
 import ProductRouter from "./routes/products.routes";
